fee8b61a7fdd7247508774274bbac83aa81feddf
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-07 01:40:31 +0200

    modified:   src/app/actions/auth.js

--- src/app/actions/user.js
+++ src/app/actions/user.js
@@ -3,18 +3,21 @@ import {
   REQUEST_USER,
   RECEIVE_USER,
 } from '../constants/actionTypes';
+import { login } from './auth';
 
 export const fetchProfile = () => async (dispatch) => {
   dispatch({ type: REQUEST_USER });
 
   try {
-    const res = await api.get('/profile', { withCredentials: true });
+    const res = await api.get('/api/profile', { withCredentials: true });
     dispatch({ type: RECEIVE_USER, payload: res.data });
     return res.data;
   } catch (e) {
     // if (e.response?.status === 401 || e.response?.status === 302) {
       // dispatch({ type: AUTH_ERROR });
-      window.location.href = `/oauth2/authorization/google`;
+      login();
+      // window.location.href = `http://34.118.68.15.nip.io/oauth2/authorization/google`;
+
     // } 
   }
 };
