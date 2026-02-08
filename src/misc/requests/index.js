4290d3b4dffda9d8c27c5f2190c0d3317fb38c34
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-08 18:03:15 +0200

    changed path

--- src/misc/requests/index.js
+++ src/misc/requests/index.js
@@ -11,7 +11,7 @@ api.interceptors.response.use(
   error => {
     if (error.response?.status === 401) {
       console.warn('User is not authenticated (401)');
-      window.location.href = `https://34.118.68.15.nip.io/api/oauth2/authorization/google`;
+      window.location.href = `https://34.118.68.15.nip.io/oauth2/authorization/google`;
     }
     return Promise.reject(error);
   }
